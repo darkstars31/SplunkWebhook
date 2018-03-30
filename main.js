2df65c4d61879ad6b4bb5ca5646d31efd7c6f2ee
AMSanti <darkstars31@gmail.com>
2018-03-30 15:25:36 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+var config  = require('./config');
+var express = require('express');
+var log4js  = require('log4js');
+var app     = express();
+var helmet = require('helmet');
+var http = require('request-promise');
+
+var logger = log4js.getLogger('api');
+log4js.configure(config.log4jsConfig);
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded());
+
+console.info(`Webhook Service is up on port: ${config.express.port}`);
+
+app.post("/v1/webhook", (req, res, next) => {
+
+	var options = {
+				method: "POST",
+				uri: `https://maker.ifttt.com/trigger/notification/with/key/${config.iftttKey}`,
+				body: {	value1: req.body.search_name },
+				json: true
+			};
+			http(options).then(()=> {console.log('Notification Triggered: '+ req.body.search_name + ' @ ' + new Date().toUTCString())})
+			.catch((e) => { logger.error("Error Sending Notification: " + req.body.search_name)});
+			
+	res.send("true");
+});
+
+
+app.listen(config.express.port);
